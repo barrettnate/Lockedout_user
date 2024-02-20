118c6439946efdd72d13cf0dc29f215c7d549634
Nate Barrett <nate_barrett@hillspet.com>
2024-02-20 15:05:43 -0600

    Clean up

--- LockedOutUser.ps1
+++ LockedOutUser.ps1
@@ -2,21 +2,24 @@
 function LockedoutUser{
     Write-Host "Check if AD user is locked out or password is expired." -ForegroundColor Green
     $userName = read-host -Prompt "What is the username to check? "
+    try{
+        get-aduser $userName -prop lockedout, passwordexpired -ErrorAction Stop
+    }
+    catch{
+        Write-Host "User not found."
+    }
 
-    get-aduser $userName -prop lockedout, passwordexpired
-
-}
 function Run_Script{
-do {
-    #Your script logic here
-    LockedoutUser
-    Write-Host " Press 'x' to exit or any other key to continue.`n" -ForegroundColor Green
+    do {
+        #Your script logic here
+        LockedoutUser
+        Write-Host " Press 'x' to exit or any other key to continue.`n" -ForegroundColor Green
     
-    #Wait for user input
-    $input = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        #Wait for user input
+        $input = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
-} while ($input.Character -ne 'x')
+    } while ($input.Character -ne 'x')
 
-Write-Host "You have exited the script." -ForegroundColor Red
+    Write-Host "You have exited the script." -ForegroundColor Red
 }
 Run_Script
